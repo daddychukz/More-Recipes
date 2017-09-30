0be8ca9b1fe77ff4ef9c13b7c3e53cee1706e6a9
daddychuks <durugo_chuks@yahoo.com>
2017-09-30 16:15:42 +0100

    create user model

---
+++ Server/src/models/user.js
@@ -0,0 +1,60 @@
+/**
+* This function creates the model of
+* Users table in the database, specifying
+* relationships, datatypes and constraints.
+* 
+*/
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Empty strings not allowed' }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: { msg: 'Enter a Valid Email' },
+      }
+    },
+    userName: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: {
+        args: [4, 5],
+        msg: 'Password must be between 4 to 5 characters at length'
+      }
+    },
+    confirmPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: {
+        args: [4, 5],
+        msg: 'Password must be between 4 to 5 characters at length'
+      }
+    },
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
