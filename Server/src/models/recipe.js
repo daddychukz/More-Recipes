7645000b0c40879597e6de55b99c7277a25327ac
daddychuks <durugo_chuks@yahoo.com>
2017-09-30 16:30:09 +0100

    refactor model to use validations

--- Server/src/models/recipe.js
+++ Server/src/models/recipe.js
@@ -1,13 +1,46 @@
 
 
-module.exports = function (sequelize, DataTypes) {
+export default (sequelize, DataTypes) => {
   const Recipe = sequelize.define('Recipe', {
-    recipeId: DataTypes.STRING,
-    userId: DataTypes.STRING,
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    upvotes: DataTypes.NUMBER,
-    views: DataTypes.NUMBER
+    recipeId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Empty strings not allowed' }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Empty strings not allowed' }
+      }
+    },
+    upvotes: {
+      type: DataTypes.NUMBER,
+      defaultValue: 0,
+      validate: {
+        notEmpty: { msg: 'Empty strings not allowed' }
+      }
+    },
+    views: {
+      type: DataTypes.NUMBER,
+      defaultValue: 0,
+      validate: {
+        notEmpty: { msg: 'Empty strings not allowed' }
+      }
+    },
   }, {
     classMethods: {
       associate(models) {
