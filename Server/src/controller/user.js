2d507054d9e6f9683b01c6126d3f3ad396c7933d
daddychuks <durugo_chuks@yahoo.com>
2017-10-02 04:14:49 +0100

    refactor code to use data from database

--- Server/src/controller/user.js
+++ Server/src/controller/user.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 import db from '../models/';
 
 const User = db.User;
+const Favorites = db.Favorites;
 
 const secret = 'andelabootcampcycle27';
 
 /* Register a User */
 const signUp = (req, res) => User
   .create({
-    // userId: req.body.userId,
     fullName: req.body.fullname,
     email: req.body.email,
     sex: req.body.sex,
@@ -60,7 +60,46 @@ const signIn = (req, res) => {
     });
 };
 
+/* Add favorites */
+const addFavorites = (req, res) => Favorites
+  .create({
+    userId: req.body.userID,
+    recipeId: req.params.recipeID,
+    category: req.body.category
+  })
+  .then(favorite => res.status(201).send({
+    favorite
+  }))
+  .catch(err => res.status(400).send(err));
+
+/* Retrieve favorites */
+const retrieveFavorites = (req, res) => {
+  Favorites
+    .findOne({
+      where: {
+        userId: req.params.userID
+      }
+    })
+    .then((recipe) => {
+      if (recipe) {
+        res.status(200).send({
+          favoriteRecipe: recipe
+        });
+      } else {
+        res.status(404).send({
+          message: 'Record not Found!'
+        });
+      }
+    })
+    .catch(() => res.status(400).send({
+      message: 'User not Found'
+    }));
+};
+
+
 export default {
   signUp,
-  signIn
+  signIn,
+  addFavorites,
+  retrieveFavorites
 };
