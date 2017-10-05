ff9feb75b76e63fe10eae55ef51eec56f53dbb9d
daddychuks <durugo_chuks@yahoo.com>
2017-10-05 04:16:47 +0100

    include fake data to test

--- Server/tests/first.js
+++ Server/tests/first.js
@@ -4,7 +4,7 @@
 import request from 'supertest';
 import chai from 'chai';
 import app from '../src/app';
-import fakeData from './helpers/dummyData';
+import fakeData from './helpers/fakeData';
 
 const expect = chai.expect;
 
