de1be117f369a6eb7776912e365d22c3d2988611
daddychuks <durugo_chuks@yahoo.com>
2017-10-05 04:18:17 +0100

    include new recipe object in fakeData file

---
+++ Server/tests/helpers/fakeData.js
@@ -0,0 +1,34 @@
+import faker from 'Faker';
+
+const fakeData = {
+
+  userOne: {
+    fullname: 'Daddychuks',
+    email: 'chuks@yahoo.com',
+    sex: 'male',
+    username: 'chuks',
+    password: 'chuks',
+    confirmPassword: 'chuks'
+  },
+
+  userTwo: {
+    fullname: faker.Name.findName(),
+    email: 'user@yahoo.com',
+    sex: 'male',
+    username: 'user',
+    password: 'user',
+    confirmPassword: 'user'
+  },
+
+  recipe: {
+    title: 'Beans',
+    description: 'This is how to prepare beans'
+  },
+
+  recipe2: {
+    title: 'Egusi',
+    description: 'This is how to prepare Egusi soup'
+  },
+};
+
+export default fakeData;
