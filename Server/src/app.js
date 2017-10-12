5e64e1bbd62bbc38a5852afe31e085a9125674e3
daddychuks <durugo_chuks@yahoo.com>
2017-10-12 12:53:07 +0100

    configure swagger for api documentation

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,15 +2,49 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
 import routes from './routes/index';
 
 
 const app = express();
 
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Chuks Recipes',
+    version: '1.0.0',
+    description: 'An Online Recipe management platform API Documentation',
+    contact: {
+      name: 'Durugo Chukwukadibia',
+      url: 'https://chuks-recipes.herokuapp.com/api/v1',
+      email: 'durugo_chuks@yahoo.com'
+    },
+  },
+  host: 'localhost:5000',
+  basePath: '/',
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['Server/dist/doc.js'],
+};
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// serve swagger
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+app.use(express.static('Server/public'));
 
 // connect all routes to application
 app.use('/api/v1/', routes);
