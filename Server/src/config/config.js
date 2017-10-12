de18fdbdfc47af98da7c50a885e37d106c31b71a
daddychuks <durugo_chuks@yahoo.com>
2017-10-12 12:03:54 +0100

    rename test db

--- Server/src/config/config.js
+++ Server/src/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   test: {
     username: 'postgres',
     password: 'hb',
-    database: 'RecipeTest',
+    database: 'recipetest',
     host: '127.0.0.1',
     dialect: 'postgres'
   },
