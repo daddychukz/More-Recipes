c4b4c478d26a29ebd8909116fae64385587d54cb
daddychuks <durugo_chuks@yahoo.com>
2017-10-14 19:07:23 +0100

    setup webpack server

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// resolve path of application
+const path = require('path');
+
+const DIST_DIR = path.resolve(__dirname, 'Client/dist');
+
+// directory to serve and transpile app from
+const SRC_DIR = path.resolve(__dirname, 'Client/src');
+
+// webpack configuration
+const config = {
+  // file to start transpiling from
+  entry: SRC_DIR + '/app.js',
+  output: {
+    path: DIST_DIR + '/app',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  // modules for transpiling
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
