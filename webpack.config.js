d577918a604cd2d6402e991f575b53c2331a960d
daddychuks <durugo_chuks@yahoo.com>
2017-10-15 23:33:42 +0100

    configure scss with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,26 @@ const config = {
   },
   // modules for transpiling
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node-modules/,
+        loader: ['style-loader', 'css-loader']
+      },
       {
         test: /\.js?/,
         include: SRC_DIR,
@@ -28,6 +47,10 @@ const config = {
         query: {
           presets: ['react', 'es2015', 'stage-2']
         }
+      },
+      {
+        test: /\.(woff|png|jpg|gif)$/,
+        loader: 'url-loader?limit=250000'
       }
     ]
   }
