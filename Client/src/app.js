526f732db932734e33200ffe2d66a4a729be7a4a
daddychuks <durugo_chuks@yahoo.com>
2017-10-17 02:03:50 +0100

    configure redux devtools

--- Client/src/app.js
+++ Client/src/app.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './routes';
 import './utils/css/style.scss';
-import rootReducer from './rootReducer';
+import configStore from './store/configStore';
 
-const store = createStore(
-  rootReducer, composeWithDevTools(applyMiddleware(thunk))
-);
+const store = configStore();
 
 
 ReactDOM.render(
