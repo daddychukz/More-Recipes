6fabd77f0ee2bd8ee70c21e8814b680e215730f5
daddychuks <durugo_chuks@yahoo.com>
2017-10-24 22:35:23 +0100

    add create_recipe action type

--- Client/src/actions/types.js
+++ Client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const USER_LOGGED_IN = 'USER_LOGGED_IN';
 export const CREATE_USER = 'CREATE_USER';
+export const CREATE_RECIPE = 'CREATE_RECIPE';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
