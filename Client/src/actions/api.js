82d9564dcf5ae26b08d8db29a1fd4ef50376183c
daddychuks <durugo_chuks@yahoo.com>
2017-10-25 09:03:15 +0100

    remove unused  files

--- Client/src/actions/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default {
-  user: {
-    login: credentials =>
-      axios.post('http://localhost:5000/api/v1/users/signin', { credentials }).then(res => res.data.user)
-  }
-};
