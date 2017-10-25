803da8f1b6e6215edaae59131375a2c5b91f2c7d
daddychuks <durugo_chuks@yahoo.com>
2017-10-25 09:09:17 +0100

    remove unused route path

--- Client/src/routes/index.js
+++ Client/src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/pages/Home';
-import LoginPage from '../components/pages/LoginPage';
 import NotFound from '../components/pages/NotFound';
 import RecipeBox from '../components/pages/RecipeBox';
 import AddRecipe from '../components/pages/AddRecipe';
@@ -10,7 +9,6 @@ const myRoutes= () => (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={LoginPage} />
       <Route path="/recipe-box" exact component={RecipeBox} />
       <Route path="/add-recipe" exact component={AddRecipe} />
       <Route component={NotFound}/>
