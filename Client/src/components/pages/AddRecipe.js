7f8764d8167ec2d8807bede6368ea699148fb831
daddychuks <durugo_chuks@yahoo.com>
2017-10-25 09:05:14 +0100

    refactor header import

--- Client/src/components/pages/AddRecipe.js
+++ Client/src/components/pages/AddRecipe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header } from './Header';
+import Header from './Header';
 import { Footer } from './Footer';
 import RecipeForm from '../forms/RecipeForm';
 
