2e1049cfa810260d04da5e13cd29f544e2a1f07e
daddychuks <durugo_chuks@yahoo.com>
2017-10-30 21:44:07 +0100

    refactor header imports

--- Client/src/components/pages/NotFound.js
+++ Client/src/components/pages/NotFound.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Header } from './Header';
-import { Footer } from './Footer';
+import Header from './Header';
+import Footer from './Footer';
 
-const NotFound = () => {
+export const NotFound = () => {
     return (
         <div>
             <Header />
