b419f7c5da843c79b9b0c4a9a85da4578e62e102
daddychuks <durugo_chuks@yahoo.com>
2017-10-30 21:43:50 +0100

    add logout function

--- Client/src/components/pages/Header.js
+++ Client/src/components/pages/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import{ logout } from '../../actions/userActions';
+import { logout } from '../../actions/userActions';
 
 
 class Header extends React.Component {
