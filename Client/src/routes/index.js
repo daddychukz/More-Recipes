765fb73a253202ba48b60d0b458320783f89b401
daddychuks <durugo_chuks@yahoo.com>
2017-10-30 21:45:32 +0100

    include recipe detail route

--- Client/src/routes/index.js
+++ Client/src/routes/index.js
@@ -4,6 +4,7 @@ import Home from '../components/pages/Home';
 import NotFound from '../components/pages/NotFound';
 import RecipeBox from '../components/pages/RecipeBox';
 import AddRecipe from '../components/pages/AddRecipe';
+import RecipeDetail from '../components/pages/RecipeDetail';
 import requireAuth from '../utils/requireAuth';
 import noAuth from '../utils/noAuth';
 
@@ -13,6 +14,7 @@ const myRoutes= () => (
       <Route path="/" exact component={noAuth(Home)} />
       <Route path="/recipe-box" exact component={RecipeBox} />
       <Route path="/add-recipe" exact component={requireAuth(AddRecipe)} />
+      <Route path="/recipe/:recipeId" exact component={requireAuth(RecipeDetail)} />
       <Route component={NotFound}/>
     </Switch>
   </Router>
