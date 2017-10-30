ac2773ac2769bd1b66798f18f33f99106f9a9537
daddychuks <durugo_chuks@yahoo.com>
2017-10-30 21:47:04 +0100

    refactor authorization header

--- Server/src/middleware/auth.js
+++ Server/src/middleware/auth.js
@@ -6,8 +6,8 @@ const secret = process.env.SECRET;
 const Auth = {
   // function to authenticate access to users with a token
   verify(req, res, next) {
-    // const token = req.headers.authorization;
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization;
+    // const token = req.headers.authorization.split(' ')[1];
     if (token) {
       jwt.verify(token, secret, (err, decoded) => {
         if (err) {
