5b78c49093860a68d23d46af2e1e4dcb9d22fa80
daddychuks <durugo_chuks@yahoo.com>
2017-11-07 06:20:03 +0100

    create review action

---
+++ Client/src/actions/reviewActions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import toastr from 'toastr';
+import * as types from './types';
+
+
+export const reviewRecipe = (recipeId, reviews) => (dispatch) => {
+  return axios.post(`http://localhost:5000/api/v1/recipes/${recipeId}/reviews`, reviews).then((response) => {
+    toastr.success('Review Successfully added');
+    console.log(response.data.rev);
+    dispatch({
+      type: types.REVIEW_RECIPE,
+      payload: reviews
+    });
+  });
+};
+
+export const viewAllReviews = () => (dispatch) => {
+  return axios.get('http://localhost:5000/api/v1/reviews');
+};
