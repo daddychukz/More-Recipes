c9f968d3390c59d1311c101f8805327d38b3f66c
daddychuks <durugo_chuks@yahoo.com>
2018-02-13 17:39:32 +0100

    refactor validation

--- Server/src/controller/HandleErrors/errorHandling.js
+++ Server/src/controller/HandleErrors/errorHandling.js
@@ -1,29 +1,5 @@
 
 const errorHandling = {
-  validateSignupErrors(error, response) {
-    const errors = {};
-    if (error.errors[0].message === 'username must be unique') {
-      errors.error = { message: 'Username already exists' };
-      return response.status(409).json(errors);
-    }
-    if (error.errors[0].message === 'email must be unique') {
-      errors.error = { message: 'Email already exists' };
-      return response.status(409).json(errors);
-    }
-    if (error.errors[0].message === 'Enter a Valid Email') {
-      errors.error = { message: 'Not an email' };
-      return response.status(409).json(errors);
-    }
-    if (error.errors[0].message === 'Full name must only contain letters') {
-      errors.error = { message: 'Full name must only contain letters' };
-      return response.status(400).json(errors);
-    }
-    if (!errors.error) {
-      errors.error = { message: error.errors[0].message };
-      return response.status(400).json(errors);
-    }
-  },
-
   validateRecipeIdErrors(error, response) {
     const errors = {};
     if (error.parent.routine === 'string_to_uuid') {
